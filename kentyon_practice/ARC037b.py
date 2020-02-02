b3a38036496e9d82d9066556030a4de604c30a1b
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-02-02 18:26:29 +0900

    4

--- kentyon_practice/ARC037b.py
+++ kentyon_practice/ARC037b.py
@@ -30,3 +30,31 @@ for k,v in ll.items():
 print(cnt)
 ------------------------------
 
+n,m = map(int, input().split())
+ll = [[] * n for _ in range(n)]
+passed = [False for _ in range(n)]
+for _ in range(m):
+    a,b = map(int, input().split())
+    a -= 1
+    b -= 1
+    ll[a].append(b)
+    ll[b].append(a)
+
+def dfs(top,pre):
+    global flag
+    for tt in ll[top]:
+        if tt != pre:
+            if passed[tt]:
+                flag = False
+            else:
+                passed[tt] = True
+                dfs(tt, top)
+cnt = 0
+for i in range(n):
+    if not passed[i]:
+        passed[i] = True
+        flag = True 
+        dfs(i,-1)
+        if flag:
+            cnt += 1
+print(cnt)
